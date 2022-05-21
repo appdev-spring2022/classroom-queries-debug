93f53075d78c904308aba7759f86a90589070370
utkarshgarg92 <ugarg@chicagobooth.edu>
2022-05-21 02:17:50 +0000

    V2

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,19 +1,20 @@
 class StudentsController < ApplicationController
   def index
-    matching_students = Student.all
+    @matching_students = Student.all
 
-    @list_of_students = matching_students.order({ :created_at => :desc })
+    #@list_of_students = matching_students.order({ :created_at => :desc })
 
     render({ :template => "students/index.html.erb" })
   end
 
   def show
-    the_id = params.fetch("id")
+    the_id = params.fetch("path_id")
 
-    matching_students = Enrollment.where({ :id => the_id })
+    matching_students = Student.where({ :id => the_id })
 
     @the_student = matching_students.at(0)
 
     render({ :template => "students/show.html.erb" })
   end
+
 end
